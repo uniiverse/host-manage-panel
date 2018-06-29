9b5e213c36a75dbf7ce050c7ed2c85364963c75d
Kyle White <kylewhite@gmail.com>
2018-06-29 15:03:07 -0400

    Add Material and start building out UI

--- src/routes/ticket/ticket.js
+++ src/routes/ticket/ticket.js
@@ -1,4 +1,26 @@
 import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Radio from '@material-ui/core/Radio';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import Input from '@material-ui/core/Input';
+
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import EditIcon from '@material-ui/icons/Create';
+import CircularProgress from '@material-ui/core/CircularProgress';
+
+import TextField from '@material-ui/core/TextField';
+import { withStyles } from '@material-ui/core/styles';
 
 import './ticket.css';
 /*
@@ -49,8 +71,33 @@ const Ticket = () => {
 
 // TODO UI :)
 
+const styles = theme => ({
+  TicketTypeContainer: {
+    display: 'block'
+  },
+  formControl: {
+    display: 'block'
+  },
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+    width: 200,
+  },
+  menu: {
+    width: 200,
+  },
+  icon: {
+    margin: theme.spacing.unit,
+  },
+});
+
 const FAKE_DATA = {
   id: '5b30ee9b5c3e935a58b34a89',
+  title: 'My fine event',
   rates: {
     nodes: [{
       clientGroup: "EVERYWHERE",
@@ -81,14 +128,132 @@ const FAKE_DATA = {
   }
 }
 
-const Ticket = () => {
-  console.log(FAKE_DATA);
-  return (
-    <div className="wrapper">
-      {'hello world'}
-    </div>
-  );
+class Ticket extends React.Component {
+  state = {
+    ticketStatus: 'active',
+    ticketAvailability: 'everywhere',
+    eventTitle: 'My fine event'
+  };
+
+  handleTicketStatus = event => {
+    this.setState({ ticketStatus: event.target.value });
+  };
+
+  handleTicketAvailability = event => {
+    this.setState({ ticketAvailability: event.target.value });
+  };
+  
+  handleEventNameChange = event => {
+    this.setState({ eventTitle: event.target.value});
+  }
+  render() {
+    console.log(FAKE_DATA);
+    const { classes } = this.props;
+    return (
+      <div>
+        <AppBar position="static" color="default">
+          <Toolbar>
+            <Typography variant="title" color="inherit">
+              { this.state.eventTitle }
+            </Typography>
+            </Toolbar>
+            </AppBar>
+            <ExpansionPanel>
+              <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+                <Typography >Basic Info</Typography>
+              </ExpansionPanelSummary>
+              <ExpansionPanelDetails>
+                <Input
+                  id="error"
+                  label="Title"
+                  value={this.state.eventTitle}
+                  onChange={this.handleEventNameChange}
+                  endAdornment={
+                    <CircularProgress className={classes.progress} />
+                  }
+                />
+              </ExpansionPanelDetails>
+            </ExpansionPanel>
+            <ExpansionPanel>
+              <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+                <Typography >Where and when</Typography>
+              </ExpansionPanelSummary>
+              <ExpansionPanelDetails>
+                <Typography>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+                  sit amet blandit leo lobortis eget.
+                </Typography>
+              </ExpansionPanelDetails>
+            </ExpansionPanel>
+            <ExpansionPanel>
+              <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+                <Typography >Ticket Types</Typography>
+              </ExpansionPanelSummary>
+              <ExpansionPanelDetails className={classes.TicketTypeContainer}>
+                <ExpansionPanel>
+                  <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+                    <Typography >GA</Typography>
+                  </ExpansionPanelSummary>
+                  <ExpansionPanelDetails className={classes.TicketTypeContainer}>
+                    <FormControl component="fieldset" required className={classes.formControl}>
+                      <FormLabel component="legend">Max Quantity</FormLabel>
+                        <TextField
+                          id="maxQuantity"
+                          defaultValue="10"
+                          margin="normal"
+                        />
+                    Sold: 9
+                    </FormControl>
+                    <FormControl component="fieldset" required className={classes.formControl}>
+                      <FormLabel component="legend">Ticket Status</FormLabel>
+                      <RadioGroup
+                        aria-label="ticket-status"
+                        name="ticket-status"
+                        className={classes.group}
+                        value={this.state.ticketStatus}
+                        onChange={this.handleTicketStatus}
+                      >
+                        <FormControlLabel value="active" control={<Radio />} label="Active" />
+                        <FormControlLabel value="hidden" control={<Radio />} label="Hidden" />
+                        <FormControlLabel value="locked" control={<Radio />} label="Locked" />
+                      </RadioGroup>
+                    </FormControl>
+                    <FormControl component="fieldset" required className={classes.formControl}>
+                      <FormLabel component="legend">Availability</FormLabel>
+                      <RadioGroup
+                        aria-label="ticket-status"
+                        name="ticket-status"
+                        className={classes.group}
+                        value={this.state.ticketAvailability}
+                        onChange={this.handleTicketAvailability}
+                      >
+                        <FormControlLabel value="everywhere" control={<Radio />} label="Everywhere" />
+                        <FormControlLabel value="online" control={<Radio />} label="Online" />
+                        <FormControlLabel value="atdoor" control={<Radio />} label="At the door" />
+                      </RadioGroup>
+                    </FormControl>
+                  </ExpansionPanelDetails>
+                  </ExpansionPanel>
+                  <ExpansionPanel>
+                  <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+                    <Typography >GA 2</Typography>
+                  </ExpansionPanelSummary>
+                  <ExpansionPanelDetails>
+                    <Typography>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+                      sit amet blandit leo lobortis eget.
+                    </Typography>
+                  </ExpansionPanelDetails>
+                </ExpansionPanel>
+              </ExpansionPanelDetails>
+            </ExpansionPanel>
+
+          
+        
+      </div>
+      );
+  }
 };
 
 
-export default Ticket;
+export default withStyles(styles)(Ticket);
